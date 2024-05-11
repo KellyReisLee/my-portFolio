90111e3183fe7712990be525d66c3106474b3e7b
KellyReisLee <kristinaalvares28@gmail.com>
2024-05-11 17:19:51 +0100

    Changing header height and removing button from CTA section

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import CV from "../../assets/cv.png";
+
 
 const CTA = () => {
   return (
     <div>
       <div className="cta">
         {/* Here I will call for the CV in 'href' and give a attribute 'download' */}
-        <a href={CV} download className="btn">
-          Download CV
-        </a>
+
         {/* Here I will   */}
         <a href="#contact" className="btn btn-primary">
           Let's Talk
